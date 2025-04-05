221ab7323e7722fdbf28c1677c4f3ec21c5d6d93
Julio Cesar <juliocesarmartinez1344@gmail.com>
2025-04-05 01:11:49 -0500

    configured Better Auth with Google OAuth and Prisma with Turso

---
+++ src/lib/auth.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from "@prisma/client";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "../../db/instance";
+ 
+export const auth = betterAuth({
+    socialProviders: { 
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID!, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!, 
+        } 
+    }, 
+
+    database: prismaAdapter(prisma, {
+        provider: "sqlite", // or "mysql", "postgresql", ...etc
+
+    }),
+});
