a9ada78f4587e4db08a0b7e0e94d2ecc15f0f523
Julio Cesar <juliocesarmartinez1344@gmail.com>
2025-06-25 14:43:38 -0500

    auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,6 +3,10 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "../../db/instance";
  
 export const auth = betterAuth({
+    baseURL: process.env.NODE_ENV === "production" 
+        ? "https://chgroup.store" 
+        : "http://localhost:3000",
+    
     socialProviders: { 
         google: { 
             clientId: process.env.GOOGLE_CLIENT_ID!, 
@@ -12,15 +16,14 @@ export const auth = betterAuth({
 
     database: prismaAdapter(prisma, {
         provider: "sqlite", // or "mysql", "postgresql", ...etc
-
     }),
+    
     user: {
         additionalFields: {
             role: {
                 type: "string",
                 default: "user",    
                 required: true,
-                
             }
         }
     }
